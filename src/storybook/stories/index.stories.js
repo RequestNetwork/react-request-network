79ecc3ab3c2efe51a9b21967ea2049904fbe4e93
Benjamin LEVESQUE <benjamin.levesque@request.network>
2018-10-02 15:35:29 +0200

    added stories

--- src/storybook/stories/index.stories.js
+++ src/storybook/stories/index.stories.js
@@ -3,24 +3,37 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import { linkTo } from "@storybook/addon-links";
-import RequestNetworkProvider, { Consumer, Publisher } from "../../../packages/react-request-network/dist";
+import RequestNetworkProvider, {
+  Consumer,
+  Publisher
+} from "../../../packages/react-request-network/dist";
 
-storiesOf("Getting Started", module).add("Current Address", () => (
-  <RequestNetworkProvider onInit={action("connected")}>
-    {
-      <Consumer>
-        {requestNetwork => <div>{requestNetwork.currentAccount}</div>}
-      </Consumer>      
-    }
-  </RequestNetworkProvider>
-));
+storiesOf("Getting Started", module)
+  .add("Current Address", () => (
+    <RequestNetworkProvider onInit={action("connected")}>
+      {
+        <Consumer>
+          {requestNetwork => <div>{requestNetwork.currentAccount}</div>}
+        </Consumer>
+      }
+    </RequestNetworkProvider>
+  ))
+  .add("Current Network", () => (
+    <RequestNetworkProvider onInit={action("connected")}>
+      {
+        <Consumer>
+          {requestNetwork => <div>{requestNetwork.currentNetwork}</div>}
+        </Consumer>
+      }
+    </RequestNetworkProvider>
+  ))
+  .add("Is Ready", () => (
+    <RequestNetworkProvider onInit={action("connected")}>
+      {
+        <Consumer>
+          {requestNetwork => <div>{requestNetwork.isReady}</div>}
+        </Consumer>
+      }
+    </RequestNetworkProvider>
+  ));
 
-// storiesOf('Button', module)
-//   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-//   .add('with some emoji', () => (
-//     <Button onClick={action('clicked')}>
-//       <span role="img" aria-label="so cool">
-//         😀 😎 👍 💯
-//       </span>
-//     </Button>
-//   ));
