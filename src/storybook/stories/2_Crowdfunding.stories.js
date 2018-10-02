e62d292bd7e1f7874c93ac3f0dbe36e35e92c4cc
Benjamin LEVESQUE <benjamin.levesque@request.network>
2018-10-02 17:37:49 +0200

    publisher story fixed

--- src/storybook/stories/2_Crowdfunding.stories.js
+++ src/storybook/stories/2_Crowdfunding.stories.js
@@ -10,8 +10,24 @@ import RequestNetworkProvider, {
 } from "@requestnetwork/react-components";
 
 //dummy request ID from rinkeby
-const requestId =
-  "0x8fc2e7f2498f1d06461ee2d547002611b801202b000000000000000000000640";
+const requestId = "0x8fc2e7f2498f1d06461ee2d547002611b801202b000000000000000000000640";
+
+const address = "0x0e8d9cb9e11278ad6e2ba1ca90385c7295dc6532";
+
+const project = {
+    amount="100",
+    category="test category",
+    description="test description",
+    id="test id",
+    isOwner="test id owner",
+    isPublished=false,
+    logoUrl="test url",
+    paymentAddress=address,
+    projectImageUrl="test image url",
+    title="test title",
+    txHash=null,
+  };
+
 
 storiesOf("Crowdfunding", module)
   .add("Backer", () => (
@@ -22,7 +38,7 @@ storiesOf("Crowdfunding", module)
   .add("Publisher", () => (
     <RequestConsumer>
       {_ => (
-        <Publisher requestId={requestId} component={ShowAll} />
+        <Publisher project={project} component={ShowAll} />
       )}
     </RequestConsumer>
   ));
